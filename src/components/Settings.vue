770e7ba0abbd89f2f6a1f72371f0b68d83970316
Atlan <gevaliatlaniskt@gmail.com>
2019-10-26 15:08:35 +0200

    Added classic/modern style changes to modal box

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,77 +1,128 @@
 <template>
   <div class='modal-container'>
-    <div class='modal-content'>
+    <div
+      class='modal-content'
+      :class="{modern: on, classic: !on}"
+    >
       <div id='close'><span @click='closeSettings'>X</span></div>
-      <div id='theme' @click='toggleTheme'>Theme: <span :class='{selected: currentTheme}'>Modern</span><span :class='{selected: !currentTheme}'>Classic</span></div>
-      <div id='sound' @click='toggleSound'>Sound: <span :class='{selected: soundOn}'>On</span> <span :class='{selected: !soundOn}'>Off</span></div>
+      <div
+        id='theme'
+        @click='toggleTheme'
+      >Theme: <span :class='{selected: currentTheme}'>Modern</span><span :class='{selected: !currentTheme}'>Classic</span></div>
+      <div
+        id='sound'
+        @click='toggleSound'
+      >Sound: <span :class='{selected: soundOn}'>On</span> <span :class='{selected: !soundOn}'>Off</span></div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    toggleTheme() {
-      this.$store.commit('toggleTheme')
+  export default {
+    methods: {
+      toggleTheme() {
+        this.$store.commit('toggleTheme')
+      },
+      toggleSound() {
+        this.$store.commit('toggleSound')
+      },
+      closeSettings() {
+        this.$store.commit('toggleSettings')
+      }
     },
-    toggleSound() {
-      this.$store.commit('toggleSound')
-    },
-    closeSettings(){
-      this.$store.commit('toggleSettings')  
-    }
-  },
 
-  computed: {
-    currentTheme(){
+    computed: {
+      currentTheme() {
         return this.$store.state.modern
-    },
-    soundOn(){
+      },
+      soundOn() {
         return this.$store.state.soundOn
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-* {
-  font-family: PressStart2P;
-  font-size: 24px;
-  cursor: pointer;
-}
+  * {
+    cursor: pointer;
+  }
 
-.modal-container {
-  width: 900px;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 1;
-}
+  .classic .modal-container {
+    font-family: PressStart2P;
+    font-size: 1.5rem;
+  }
 
-.modal-content {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  margin: 10vh auto;
-  height: 50%;
-  width: 70%;
-  background-color: #0738b1;
-  color: yellow;
-}
+  .modern .modal-container {
+    font-family: FjallaOne;
+    font-size: 2.5rem;
+    color: seashell;
+  }
 
-#close {
-  font-size: 30px;
-  display: flex;
-  justify-content: flex-end;
-  margin: 20px;
-}
 
-div span{
+  .modal-container {
+    width: 900px;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 1;
+  }
+
+
+  .modal-content {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    margin: 10vh auto;
+    height: 50%;
+    width: 70%;
+  }
+
+  .classic .modal-content {
+    background-color: #0738b1;
+    color: yellow;
+  }
+
+  .modern .modal-content {
+    background-color: #333;
+    color: seashell;
+  }
+
+  .classic .selected {
+    color: cyan;
+    border: none;
+    background-color: navy;
+    border: solid 4px navy;
+  }
+
+  .modern .selected {
+    color: #53d8fb;
+    border-color: #53d8fb;
+    background: none;
+    border: solid 4px seashell;
+  }
+
+  .modern #close {
+    color: cyan;
+  }
+
+  .classic #close {
+    color: red;
+  }
+
+  #close {
+    font-size: 30px;
+    display: flex;
+    justify-content: flex-end;
+    margin: 20px;
+  }
+
+
+  div span {
     padding: 10px;
-}
+  }
 
-.selected{
+  .selected {
     border: 4px solid white;
-}
+  }
 </style>
--- src/components/VideoPoker.vue
+++ src/components/VideoPoker.vue
@@ -26,7 +26,7 @@
     </div>
 
     <div id="infoBar">
-      <span v-bind:style="{visibility: win === 0 ? 'hidden' : 'visible'}">WIN {{ win }}</span>
+      <span :style="{visibility: win === 0 ? 'hidden' : 'visible'}">WIN {{ win }}</span>
       <span>CREDITS {{ credits }}</span>
     </div>
     <div id="betCoinBar">
@@ -42,7 +42,8 @@
       <span
         id="bet"
         class="coin"
-      >{{ bet }}</span>
+      >
+        {{ bet }}</span>
       <span
         style="[round > 0 ? {pointerEvents: 'none'} : {pointerEvents: 'auto'}]"
         class="coin-arrow"
@@ -62,7 +63,7 @@
   </div>
 </template>
 <script>
-  import Scoretable from "../components/Scoretable.vue";
+  import Scoretable from '../components/Scoretable.vue'
   import Settings from '../components/Settings.vue'
   export default {
     components: {
@@ -71,63 +72,63 @@
     },
     computed: {
       componentOn() {
-        return this.$store.state.pokerDisplay;
+        return this.$store.state.pokerDisplay
       },
       getCombination() {
-        return this.$store.state.combination;
+        return this.$store.state.combination
       },
 
       randomCards() {
-        return this.$store.state.fiveRandomCards;
+        return this.$store.state.fiveRandomCards
       },
 
       finalCards() {
-        return this.$store.state.finalCards;
+        return this.$store.state.finalCards
       },
 
       credits() {
-        return this.$store.state.credits;
+        return this.$store.state.credits
       },
 
       bet() {
-        return this.$store.state.bet;
+        return this.$store.state.bet
       },
 
       round() {
-        return this.$store.state.round;
+        return this.$store.state.round
       },
 
       win() {
-        return this.$store.state.win;
+        return this.$store.state.win
       },
 
       on() {
-        return this.$store.state.modern;
+        return this.$store.state.modern
       },
 
       gameInfo() {
-        return this.$store.state.gameInfo;
+        return this.$store.state.gameInfo
       },
 
-      showSettings(){
+      showSettings() {
         return this.$store.state.showSettings
       }
     },
 
     methods: {
       getCards() {
-        this.$store.commit("getFiveRandomCards");
+        this.$store.commit("getFiveRandomCards")
         // this.$store.commit("calculateValue");
       },
       getMoreCards() {
-        this.$store.commit("getMoreCards");
+        this.$store.commit("getMoreCards")
       },
 
       calcValue() {
-        this.$store.commit("calculateValue");
+        this.$store.commit("calculateValue")
       },
       lockCard(id) {
-        this.$store.commit("toggleLock", id);
+        this.$store.commit("toggleLock", id)
       },
 
       insertCoin() {
@@ -142,7 +143,7 @@
         this.$store.commit("toggleTheme");
       },
 
-      toggleSettings(){
+      toggleSettings() {
         this.$store.commit('toggleSettings')
       }
     }
@@ -151,246 +152,246 @@
 
 
 <style scoped>
-#component-body {
-  margin: 0 auto;
-  display: block;
-}
+  #component-body {
+    margin: 0 auto;
+    display: block;
+  }
 
-.classic {
-  background-color: #0738b1;
-  transition: all 300ms linear;
-}
+  .classic {
+    background-color: #0738b1;
+    transition: all 300ms linear;
+  }
 
-#cards-display {
-  position: relative;
-  display: flex;
-  justify-content: space-evenly;
-  height: 30vh;
-}
+  #cards-display {
+    position: relative;
+    display: flex;
+    justify-content: space-evenly;
+    height: 30vh;
+  }
 
-.suit {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  grid-area: suit;
-  font-size: 40px;
-}
+  .suit {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    grid-area: suit;
+    font-size: 40px;
+  }
 
-.value {
-  margin-left: 2px;
-  display: flex;
-  justify-content: flex-start;
-  grid-area: value;
-}
+  .value {
+    margin-left: 2px;
+    display: flex;
+    justify-content: flex-start;
+    grid-area: value;
+  }
 
-.value2 {
-  display: flex;
-  align-items: flex-end; 
-  grid-area: value2;
-  justify-content: flex-end;
-  margin: 0;
-}
+  .value2 {
+    display: flex;
+    align-items: flex-end;
+    grid-area: value2;
+    justify-content: flex-end;
+    margin: 0;
+  }
 
-.card {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  grid-template-areas: "value" "suit" "value2";
-  cursor: pointer;
-  border-radius: 5px;
-  margin: 10px;
-  width: 20%;
-  min-height: 24vh;
-  font-size: 24px;
-  padding: 0.5rem 0.3rem;
-  margin-bottom: 2rem;
-}
+  .card {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-areas: "value""suit""value2";
+    cursor: pointer;
+    border-radius: 5px;
+    margin: 10px;
+    width: 20%;
+    min-height: 24vh;
+    font-size: 24px;
+    padding: 0.5rem 0.3rem;
+    margin-bottom: 2rem;
+  }
 
-#game-info {
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  font-size: 3rem;
-  transform: translate(-50%, -50%);
-}
+  #game-info {
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    font-size: 3rem;
+    transform: translate(-50%, -50%);
+  }
 
-.classic #game-info {
-  font-family: PressStart2P;
-  color: #fa2a26;
-  text-shadow: -4px 0 #ffff37, 0 4px #ffff37, 4px 0 #ffff37, 0 -4px #ffff37;
-}
+  .classic #game-info {
+    font-family: PressStart2P;
+    color: #fa2a26;
+    text-shadow: -4px 0 #ffff37, 0 4px #ffff37, 4px 0 #ffff37, 0 -4px #ffff37;
+  }
 
-.modern #game-info {
-  font-family: FjallaOne;
-  color: deepskyblue;
-  font-size: 4rem;
-  letter-spacing: 0.9rem;
-  -webkit-text-stroke: 2px white;
-  line-height: 4.2rem;
-}
+  .modern #game-info {
+    font-family: FjallaOne;
+    color: deepskyblue;
+    font-size: 4rem;
+    letter-spacing: 0.9rem;
+    -webkit-text-stroke: 2px white;
+    line-height: 4.2rem;
+  }
 
-.classic .card {
-  background-color: #fff;
-  font-family: PressStart2P;
-  /* transition: all 0.3s; */
-}
+  .classic .card {
+    background-color: #fff;
+    font-family: PressStart2P;
+    /* transition: all 0.3s; */
+  }
 
-.classic .card:not(.locked) {
-  border: 8px solid transparent;
-}
+  .classic .card:not(.locked) {
+    border: 8px solid transparent;
+  }
 
-.modern .card:not(.locked) {
-  border: 2px solid transparent;
-  transition: all 300ms linear;
-}
+  .modern .card:not(.locked) {
+    border: 2px solid transparent;
+    transition: all 300ms linear;
+  }
 
-.modern {
-  background-color: #aaa;
-  transition: all 300ms linear;
-}
+  .modern {
+    background-color: #aaa;
+    transition: all 300ms linear;
+  }
 
-.modern .card {
-  background-color: #eee;
-  border: 4px solid transparent;
-  font-family: Lovelo-Black;
-}
+  .modern .card {
+    background-color: #eee;
+    border: 4px solid transparent;
+    font-family: Lovelo-Black;
+  }
 
-.classic .locked {
-  border: solid 8px #fa2a26;
-}
+  .classic .locked {
+    border: solid 8px #fa2a26;
+  }
 
-.modern .locked {
-  border: solid 2px #d3728c;
-  transition: all 300ms linear;
-}
+  .modern .locked {
+    border: solid 2px #d3728c;
+    transition: all 300ms linear;
+  }
 
-#combination {
-  padding-top: 20px;
-  margin-bottom: 0.3rem;
-  line-height: 2rem;
-}
+  #combination {
+    padding-top: 20px;
+    margin-bottom: 0.3rem;
+    line-height: 2rem;
+  }
 
-.classic #combination {
-  font-size: 28px;
-  font-family: PressStart2P;
-  color: #fa2a26;
-  text-shadow: -2px 0 #ffff37, 0 2px #ffff37, 2px 0 #ffff37, 0 -2px #ffff37;
-}
+  .classic #combination {
+    font-size: 28px;
+    font-family: PressStart2P;
+    color: #fa2a26;
+    text-shadow: -2px 0 #ffff37, 0 2px #ffff37, 2px 0 #ffff37, 0 -2px #ffff37;
+  }
 
-.modern #combination {
-  font-family: FjallaOne;
-  font-size: 38px;
-  color: #dce1e9;
-}
+  .modern #combination {
+    font-family: FjallaOne;
+    font-size: 38px;
+    color: #dce1e9;
+  }
 
-#infoBar {
-  display: flex;
-  justify-content: space-around;
-  padding: 10px;
-}
+  #infoBar {
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+  }
 
-.classic #infoBar {
-  font-family: PressStart2P;
-  font-size: 24px;
-  color: #fa2a26;
-  text-shadow: -2px 0 #ffff37, 0 2px #ffff37, 2px 0 #ffff37, 0 -2px #ffff37;
-}
+  .classic #infoBar {
+    font-family: PressStart2P;
+    font-size: 24px;
+    color: #fa2a26;
+    text-shadow: -2px 0 #ffff37, 0 2px #ffff37, 2px 0 #ffff37, 0 -2px #ffff37;
+  }
 
-.modern #infoBar {
-  font-family: FjallaOne;
-  font-size: 24px;
-  color: #53d8fb;
-}
+  .modern #infoBar {
+    font-family: FjallaOne;
+    font-size: 24px;
+    color: #53d8fb;
+  }
 
-#betCoinBar {
-  cursor: pointer;
-  color: #000;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  cursor: pointer;
-  font-size: 24px;
-  padding: 10px;
-}
+  #betCoinBar {
+    cursor: pointer;
+    color: #000;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    font-size: 24px;
+    padding: 10px;
+  }
 
-#settings {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 250px;
-  height: 100px;
-  font-size: 50px;
-}
+  #settings {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 250px;
+    height: 100px;
+    font-size: 50px;
+  }
 
-#credits {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 200px;
-  height: 100px;
-}
+  #credits {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 200px;
+    height: 100px;
+  }
 
-.draw-button {
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 250px;
-  height: 100px;
-  font-size: 50px;
-}
+  .draw-button {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 250px;
+    height: 100px;
+    font-size: 50px;
+  }
 
-.coin,
-.coin-arrow {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100px;
-  height: 100px;
-  font-size: 50px;
-}
+  .coin,
+  .coin-arrow {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    font-size: 50px;
+  }
 
-.classic .coin,
-.classic .coin-arrow,
-.classic .draw-button,
-.classic #credits,
-.classic #settings,
-.classic #resultText {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100px;
-  height: 100px;
-  margin: 10px;
-  font-family: PressStart2P;
-  font-size: 40px;
-  background: none;
-  border: none;
-  color: #000;
-  border: solid yellow 5px;
-  background-color: gold;
-  border-radius: 5px;
-}
+  .classic .coin,
+  .classic .coin-arrow,
+  .classic .draw-button,
+  .classic #credits,
+  .classic #settings,
+  .classic #resultText {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    font-family: PressStart2P;
+    font-size: 40px;
+    background: none;
+    border: none;
+    color: #000;
+    border: solid yellow 5px;
+    background-color: gold;
+    border-radius: 5px;
+  }
 
-.classic .draw-button {
-  width: 200px;
-}
+  .classic .draw-button {
+    width: 200px;
+  }
 
-.classic #settings {
-  width: 200px;
-  font-size: 20px;
+  .classic #settings {
+    width: 200px;
+    font-size: 20px;
 
-}
+  }
 
-.modern .coin,
-.modern .coin-arrow,
-.modern .draw-button,
-.modern #credits,
-.modern #settings {
-  font-family: FjallaOne;
-}
+  .modern .coin,
+  .modern .coin-arrow,
+  .modern .draw-button,
+  .modern #credits,
+  .modern #settings {
+    font-family: FjallaOne;
+  }
 
-/* .classic .draw-button {
+  /* .classic .draw-button {
   .modern .card:not(.locked) {
     border: 2px solid transparent;
   }
